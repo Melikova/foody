b2b297e1bd957e58a6e032add4ab4d54cd291f28
seymur2519 <seymur2526@gmail.com>
2023-09-05 21:06:16 +0300

    update layout and link tailwind css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
-import { Header } from "./components/common/Header/Header";
 
 function App() {
   return (
     <>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
