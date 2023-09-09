3560c9f458b35fe8c76bdb259dc30bddfab0f590
ibrahimseyidov <ibrahimseyidov72@gmail.com>
2023-09-09 21:45:24 +0400

    Fixed Folder Structure

--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Home = () => {
-  return (
-    <div>
-      <h1 className="mt-57px ml-32px font-logoFont font-weight800 text-28 text-logocolor">Foody<span className="text-logodotcolor">.</span></h1>
-    </div>
-  )
-}
--- src/components/pages/FirstPage/FirstPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const FirstPage = () => {
-  return (
-    <div>First Page</div>
-  )
-}
