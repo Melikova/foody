0cccc45419a0cb00e00a78d4e5391546487bb85f
seymur2519 <seymur2526@gmail.com>
2023-09-15 02:07:02 +0300

    completed design and firebase connection

---
+++ src/server/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+const firebaseConfig = {
+  apiKey: "AIzaSyA_UusQ_6EAZDtDaPyDfyGgy2RUw5HsVaA",
+  authDomain: "foody-72ed4.firebaseapp.com",
+  projectId: "foody-72ed4",
+  storageBucket: "foody-72ed4.appspot.com",
+  messagingSenderId: "429218439195",
+  appId: "1:429218439195:web:d0e3f4e0fd680b7cecd8be"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+const signIn = async (email,password)=>{
+  await signInWithEmailAndPassword(auth,email,password)
+}
+export default signIn
