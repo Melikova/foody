d5b490005504fa666494348b5dd6f26759183dd7
Gulkhanim <guluhasanzade7@gmail.com>
2023-09-17 10:39:28 +0400

    category page has created

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,10 +1,11 @@
+import Catagory from 'components/Catagory'
 import AdminLayout from 'layouts/AdminLayout'
 import React from 'react'
 
 const Category = () => {
   return (
     <AdminLayout>
-      
+      <Catagory/>
     </AdminLayout>
   )
 }
